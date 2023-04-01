96c47e1819a147c83e7e09208326bcbb83b6891b
Eman Gamal <emangaamal@gmail.com>
2023-04-01 13:46:15 +0200

    Lab01

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title') Create @endsection
+
+@section('content')
+    <form>
+        <div class="mb-3">
+            <label class="form-label">Title</label>
+            <input  type="text" class="form-control" >
+        </div>
+        <div class="mb-3">
+            <label  class="form-label">Description</label>
+            <textarea class="form-control"  rows="3"></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label  class="form-label">Post Creator</label>
+            <select class="form-control">
+                <option value="1">Eman</option>
+                <option value="2">Mai</option>
+            </select>
+        </div>
+
+        <a href="/posts"><button type="button" class="btn btn-success">Submit</button></a>
+    </form>
+@endsection
